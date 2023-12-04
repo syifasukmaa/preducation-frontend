ce7e4cd9ac2915cbb3fe8fd8532d53fd7db964d6
mnuralim <shadownur345.com>
2023-12-04 21:39:06 +0800

    add next auth configuration

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
-const path = require('path');
+const path = require("path");
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
     API_URL: process.env.API_URL,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
   },
   webpack: (config) => {
-    config.resolve.alias['@'] = path.resolve(__dirname);
+    config.resolve.alias["@"] = path.resolve(__dirname);
     return config;
   },
 };
---
+++ utils/swr.js
@@ -0,0 +1,5 @@
+const fetcher = async (url) => {
+  const res = await fetch(url);
+  const data = await res.json();
+  return data;
+};
