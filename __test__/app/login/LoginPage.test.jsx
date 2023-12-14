d9165f07a0b8aba4ad72eae3b9a9413532d56580
mnuralim <shadownur345.com>
2023-12-15 01:23:41 +0800

    setup unit testing and add login page test

---
+++ __test__/app/login/LoginPage.test.jsx
@@ -0,0 +1,56 @@
+import LoginPage from '@/app/login/page'
+import { fireEvent, render, screen, waitFor } from '@testing-library/react'
+import { signIn } from 'next-auth/react'
+
+jest.mock('next/navigation', () => ({
+  useRouter: jest.fn(() => ({ push: jest.fn() })),
+}))
+
+jest.mock('next-auth/react', () => ({
+  signIn: jest.fn(),
+}))
+
+describe('Login Page', () => {
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
+  it('Should render', () => {
+    const page = render(<LoginPage />)
+    expect(page).toMatchSnapshot()
+  })
+
+  it('handles form submission successfully', async () => {
+    signIn.mockResolvedValueOnce({ ok: true })
+    render(<LoginPage />)
+
+    fireEvent.change(screen.getByPlaceholderText('ID Admin'), { target: { value: 'admin_user' } })
+    fireEvent.change(screen.getByPlaceholderText('Password'), { target: { value: 'adminpass' } })
+
+    fireEvent.click(screen.getByText('Masuk'))
+
+    await waitFor(() => {
+      expect(signIn).toHaveBeenCalledWith('credentials', {
+        username: 'admin_user',
+        password: 'adminpass',
+        redirect: false,
+      })
+      expect(screen.getByText('Anda berhasil masuk!'))
+      expect(require('next/navigation').useRouter().push('/admin/dashboard'))
+    })
+  })
+
+  it('handles form submission failure', async () => {
+    signIn.mockResolvedValueOnce({ ok: false })
+    render(<LoginPage />)
+
+    fireEvent.change(screen.getByPlaceholderText('ID Admin'), { target: { value: 'wrong_admin' } })
+    fireEvent.change(screen.getByPlaceholderText('Password'), { target: { value: 'wrong_pass' } })
+
+    fireEvent.click(screen.getByText('Masuk'))
+
+    await waitFor(() => {
+      const errorMessage = screen.getByText('Username atau password salah')
+      expect(errorMessage).toBeInTheDocument()
+    })
+  })
+})
---
+++ jest.config.js
@@ -0,0 +1,25 @@
+const nextJest = require('next/jest')
+
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: './',
+})
+
+// Add any custom config to be passed to Jest
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  testEnvironment: 'jsdom',
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '**/*.{js,jsx,ts,tsx}',
+    '!**/*.d.ts',
+    '!**/node_modules/**',
+    '!**/coverage/**',
+    '!<rootDir>/.next/**',
+    '!<rootDir>/*.config.js',
+    '!middleware.js',
+  ],
+}
+
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+module.exports = createJestConfig(customJestConfig)
