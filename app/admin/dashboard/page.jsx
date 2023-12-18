7d500eca1b8301f5201fed5620e8ba202bbbb020
syifa sukma <syifasukmaramadhani@gmail.com>
2023-12-18 22:52:30 +0700

    improvement landing page and loading payment

--- app/admin/dashboard/page.jsx
+++ app/admin/dashboard/page.jsx
@@ -91,14 +91,14 @@ export default function Page() {
                 className="item-filter"
                 onClick={() => filterCourses('Paid')}
               >
-                Paid
+                SUDAH BAYAR
               </div>
 
               <div
                 className="item-filter"
                 onClick={() => filterCourses('On Progress')}
               >
-                On Progress
+                BELUM BAYAR
               </div>
             </FilterPopup>
           </div>
@@ -119,14 +119,8 @@ export default function Page() {
               </tr>
             </thead>
 
-            <tbody className="text-gray-700 whitespace-nowrap text-[10px]">
-              {isLoading ? (
-                <>
-                  {[...Array(3)].map((_, index) => (
-                    <PaymentLoading key={index} />
-                  ))}
-                </>
-              ) : error ? (
+            {error ? (
+              <tbody>
                 <tr>
                   <td
                     colSpan="7"
@@ -137,27 +131,32 @@ export default function Page() {
                     </div>
                   </td>
                 </tr>
-              ) : payments ? (
-                payments.map((payment) => (
-                  <tr key={payment._id}>
-                    <td className="px-4 py-4 font-bold text-gray-05">{payment.userId.username}</td>
-                    <td className="py-3 pl-4 pr-3 font-bold text-gray-05">{payment.courseId.category.name}</td>
-                    <td className="px-4 py-3 font-bold text-gray-04">{payment.courseId.level}</td>
+              </tbody>
+            ) : payments ? (
+              payments.map((payment) => (
+                <tbody
+                  key={payment._id}
+                  className="text-gray-700 whitespace-nowrap text-[10px]"
+                >
+                  <tr>
+                    <td className="px-4 py-4 font-bold text-gray-05 w-[15%]">{payment.userId.username}</td>
+                    <td className="py-3 pl-4 pr-3 font-bold text-gray-05 w-[17%]">{payment.courseId.category.name}</td>
+                    <td className="px-4 py-3 font-bold text-gray-04 w-[15%]">{payment.courseId.level}</td>
                     <td
-                      className={`py-3 px-4 font-bold ${
+                      className={`py-3 px-4 font-bold w-[15%] ${
                         payment.status === 'On Progress' ? 'text-alert-red' : 'text-alert-green'
                       }`}
                     >
-                      {payment.status}
+                      {payment.status === 'On Progress' ? 'BELUM BAYAR' : 'SUDAH BAYAR'}
                     </td>
-                    <td className="px-4 py-3 font-bold lg:pl-4 lg:pr-0 text-gray-04">{payment.paymentType}</td>
+                    <td className="px-4 py-3 font-bold lg:pl-4 lg:pr-0 text-gray-04 w-[20%]">{payment.paymentType}</td>
                     <td className="px-4 py-3 pl-4 font-bold lg:pl-0 lg:pr-1 text-gray-05">{payment.createdAt}</td>
                   </tr>
-                ))
-              ) : (
-                [...Array(5)].map((_, index) => <PaymentLoading key={index} />)
-              )}
-            </tbody>
+                </tbody>
+              ))
+            ) : (
+              [...Array(5)].map((_, index) => <PaymentLoading key={index} />)
+            )}
           </table>
         </div>
       </div>
