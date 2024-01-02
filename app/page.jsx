03dd0e710096f46ed282853a144a1c2718c390ba
mnuralim <shadownur345.com>
2024-01-02 14:06:26 +0800

    update

--- app/page.jsx
+++ app/page.jsx
@@ -3,9 +3,9 @@ import NavBar from '@/components/navbar/Navbarlp'
 import Footer from '@/components/footer/Footer'
 import Review from '@/components/landingPage/Review'
 import CardCourse from '@/components/landingPage/CardCourse'
-import Home from '@/components/LandingPage/Home'
 import Download from '@/components/landingPage/Download'
 import { getAllCourse } from '@/utils/fetch'
+import Home from '@/components/landingPage/Home'
 
 const LandingPage = async () => {
   const getCourseData = await getAllCourse()
