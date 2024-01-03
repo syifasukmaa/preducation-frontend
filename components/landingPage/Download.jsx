75ef92d13c4036662016d511aa7d0d11c3edfd4c
syifa sukma <syifasukmaramadhani@gmail.com>
2024-01-03 19:34:46 +0700

    added link predecution app and changed filter

--- components/landingPage/Download.jsx
+++ components/landingPage/Download.jsx
@@ -1,38 +1,53 @@
-'use client'
-import React from 'react'
-import { motion } from 'framer-motion'
-import { slideIn } from '@/utils/motion'
-import Image from 'next/image'
+'use client';
+import React from 'react';
+import { motion } from 'framer-motion';
+import { slideIn } from '@/utils/motion';
+import Image from 'next/image';
 
 const Download = () => {
   return (
-    <div id="download" className="flex p-3 mt-10 mb-8">
-      <div className="flex mx-auto">
+    <div
+      id='download'
+      className='flex p-3 mt-10 mb-8'
+    >
+      <div className='flex mx-auto'>
         <motion.div
           variants={slideIn('left', 'tween', 0.5, 1.5)}
-          initial="hidden"
-          whileInView="show"
-          className="flex flex-col mx-auto relative p-5 mt-[50px]"
+          initial='hidden'
+          whileInView='show'
+          className='flex flex-col mx-auto relative p-5 mt-[50px]'
         >
-          <h1 className="font-bold text-orange-05 md:text-[32px] ">Dapatkan Aplikasi Kami Di</h1>
+          <h1 className='font-bold text-orange-05 md:text-[32px] '>Dapatkan Aplikasi Kami Di</h1>
           <div>
-            <a href="#">
+            <a
+              href='https://drive.google.com/file/d/1RFhv-nI7x74oNpBPk6Hhag0j6Wb27yRk/view?usp=sharing'
+              target='_blank'
+            >
               <Image
-                src="/googlePlay.svg"
+                src='/googlePlay.svg'
                 width={200}
                 height={200}
-                alt="gambar google play"
-                className="float-right w-[200px] h-[200px]"
+                alt='gambar google play'
+                className='float-right w-[200px] h-[200px]'
               />
             </a>
           </div>
         </motion.div>
-        <motion.div variants={slideIn('right', 'tween', 0.5, 1.5)} initial="hidden" whileInView="show">
-          <Image src="/jjj.png" height={200} width={200} alt="" />
+        <motion.div
+          variants={slideIn('right', 'tween', 0.5, 1.5)}
+          initial='hidden'
+          whileInView='show'
+        >
+          <Image
+            src='/jjj.png'
+            height={200}
+            width={200}
+            alt=''
+          />
         </motion.div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Download
+export default Download;
